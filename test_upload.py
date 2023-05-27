2e30126b4f533b8962f13ebd81e762802ede40d4
Max Gulda <gulda@Macbook-Pro-16.local>
2023-05-27 14:51:06 +0200

    d

--- test_upload.py
+++ test_upload.py
@@ -2,7 +2,8 @@ import os
 from couchbase.cluster import Cluster
 from couchbase.options import ClusterOptions
 from couchbase.auth import PasswordAuthenticator
-from couchbase.transcoder import RawJSONTranscoder
+from couchbase.transcoder import RawJSONTranscoder, JSONTranscoder
+import base64
 
 # Couchbase connection parameters
 server_url = 'couchbases://cb.a6cm4sbxf8i5qvak.cloud.couchbase.com'
@@ -10,25 +11,29 @@ bucket_name = 'Main'
 accessname = 'FACE'
 secret = 'Flempan123!'
 
-def upload(file, document_id):
-    
-    document_path = file
-
+def upload(files, document_id):
     try:
         # Connect to the Couchbase cluster
         cluster = Cluster(server_url, ClusterOptions(
             authenticator=PasswordAuthenticator(accessname, secret),
-            transcoder=RawJSONTranscoder()
+            transcoder=JSONTranscoder()
         ))
         bucket = cluster.bucket(bucket_name)
         collection = bucket.default_collection()
 
-        # Read the pickle file
-        with open(document_path, 'rb') as file:
-            document_data = file.read()
+        # Create a dictionary to hold the file data
+        file_data_dict = {}
 
-        # Insert the document
-        collection.upsert(document_id, document_data)
+        # Read each JPG file and store base64 encoded data in the dictionary
+        for file in files:
+            with open(file, 'rb') as file_obj:
+                file_data = file_obj.read()
+                file_data_base64 = base64.b64encode(file_data).decode('utf-8')
+                file_name = file.split('/')[-1]
+                file_data_dict[file_name] = file_data_base64
+
+        # Insert the dictionary of file data as a single document
+        collection.upsert(document_id, file_data_dict)
 
         print('Document uploaded successfully.')
 
@@ -36,7 +41,17 @@ def upload(file, document_id):
         print(f'Error uploading document: {e}')
 
 
-upload('Data/face_embeddings_done_4classes.npz', 'embeddings')
-upload('Data/svm_model_160x160.pkl', 'svm')
 
-upload('dataset/Max_Gulda/0.jpg', 'bild')
+
+
+# List of JPG files to upload
+files = ['dataset/Max_Gulda/0.jpg', 'dataset/Max_Gulda/1.jpg']
+
+# Specify the document ID
+document_id = 'max_bild'
+
+
+
+# Call the modified upload function with the list of files
+upload(files, document_id)
+
