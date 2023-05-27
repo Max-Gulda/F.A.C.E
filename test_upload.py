d31d7a4c4390bb6d777914960ec84f7f8d573732
Max Gulda <gulda@Macbook-Pro-16.local>
2023-05-27 15:23:25 +0200

    d

--- test_upload.py
+++ test_upload.py
@@ -19,7 +19,7 @@ def upload(files, document_id):
             transcoder=JSONTranscoder()
         ))
         bucket = cluster.bucket(bucket_name)
-        collection = bucket.default_collection()
+        collection = bucket.collection('User_jpgs')
 
         # Create a dictionary to hold the file data
         file_data_dict = {}
@@ -40,18 +40,97 @@ def upload(files, document_id):
     except Exception as e:
         print(f'Error uploading document: {e}')
 
+def upload_folder(folder_path, document_prefix):
+    try:
+        # Get the list of JPG files in the folder
+        file_names = [f for f in os.listdir(folder_path) if f.lower().endswith(('.jpg', '.jpeg'))]
+
+        # Accumulate the file paths in a list
+        file_paths = []
+        for file_name in file_names:
+            file_path = os.path.join(folder_path, file_name)
+            file_paths.append(file_path)
+            print(file_path)
+
+        # Upload the list of files as a single document
+        document_id = document_prefix
+        upload(file_paths, document_id)
+
+    except Exception as e:
+        print(f'Error uploading folder: {e}')
+
+def upload_dataset(dataset_path):
+    try:
+        # Get the list of subdirectories in the dataset folder
+        subdirs = [d for d in os.listdir(dataset_path) if os.path.isdir(os.path.join(dataset_path, d))]
+
+        # Iterate over the subdirectories and upload each one
+        for subdir in subdirs:
+            folder_path = os.path.join(dataset_path, subdir)
+            upload_folder(folder_path, subdir)
+
+    except Exception as e:
+        print(f'Error uploading dataset: {e}')
 
 
+def upload_npz():
+    file_path = 'Data/face_embeddings_done_4classes.npz'
+    collection_name = 'NPZ'
+    try:
+        # Connect to the Couchbase cluster
+        cluster = Cluster(server_url, ClusterOptions(
+            authenticator=PasswordAuthenticator(accessname, secret),
+            transcoder=RawJSONTranscoder()
+        ))
+        bucket = cluster.bucket(bucket_name)
+        collection = bucket.collection(collection_name)
+
+        # Read the NPZ file
+        with open(file_path, 'rb') as file_obj:
+            file_data = file_obj.read()
+
+        # Insert the NPZ file as a document
+        collection.upsert(file_path, file_data)
+
+        print('NPZ file uploaded successfully.')
+
+    except Exception as e:
+        print(f'Error uploading NPZ file: {e}')
+
+def upload_pkl():
+    file_path = 'Data/svm_model_160x160.pkl'
+    collection_name = 'PKL'
+    try:
+        # Connect to the Couchbase cluster
+        cluster = Cluster(server_url, ClusterOptions(
+            authenticator=PasswordAuthenticator(accessname, secret),
+            transcoder=RawJSONTranscoder()
+        ))
+        bucket = cluster.bucket(bucket_name)
+        collection = bucket.collection(collection_name)
+
+        # Read the NPZ file
+        with open(file_path, 'rb') as file_obj:
+            file_data = file_obj.read()
+
+        # Insert the NPZ file as a document
+        collection.upsert(file_path, file_data)
+
+        print('PKL file uploaded successfully.')
+
+    except Exception as e:
+        print(f'Error uploading PKL file: {e}')
+
+def upload_neural_network():
+    upload_npz()
+    upload_pkl()
 
 
-# List of JPG files to upload
-files = ['dataset/Max_Gulda/0.jpg', 'dataset/Max_Gulda/1.jpg']
+upload_neural_network()
 
-# Specify the document ID
-document_id = 'max_bild'
+# Dataset path and collection prefix
+#dataset_path = 'dataset'
 
-
-
-# Call the modified upload function with the list of files
-upload(files, document_id)
+# Call the function to upload each subdirectory as a separate document
+#upload_dataset(dataset_path)
 
